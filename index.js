89199c57ba8ef1a67b9068c7ff443771da1739dc
Pavlina Mullerova <Pauline@pavlinas-macbook-air.home>
2020-11-18 17:27:19 +0100

    pridani casu ted

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-'use-strict'
+'use-strict';
+
+// console.log(Date.now() / 1000);
+
+const btnStartTime = document.querySelector('#startTime');
+
+btnStartTime.addEventListener('click', () => {
+  console.log(Date.now() / 1000);
+});
